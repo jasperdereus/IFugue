c4a72f14c9a09d7f80ed96b83661c49396d7bee0
ricrcr <jasperdereus@gmail.com>
2017-03-04 16:48:06 +0100

    Add files via upload

---
+++ IFugue.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 26 15:08:27 2017
+
+"""
+
+
+from music21 import *
+#configure.run()
+
+b = corpus.parse('bach/bwv66.6')
+b.show() 
+
+
+b.show('midi')
+b.show()
+#b.show()
+#b.show('midi')
